f76b24e7f47d3d886630438f6e2f24170f367792
Roy <loganroyjr4@gmail.com>
2022-09-08 18:19:39 -0500

    added id to remaing <textarea in HTML ... added var in js for corresponding <textarea

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ currentDateTime();
 
 // adding click event to save button ...function to save to local storage
 $('.saveBtn').click(function(){
-    var nineAm = document.getElementById('9am').value;
-    localStorage.setItem('9am', nineAm);
+    var nineAm = document.getElementById('nineAm').value;
+    localStorage.setItem('nineAm', nineAm);
+    var tenAm = document.getElementById('tenAm').value;
+    localStorage.setItem('tenAm', tenAm);
+    var elevenAm = document.getElementById('elevenAm').value;
+    localStorage.setItem('elevenAm', elevenAm);
+    var noon = document.getElementById('noon').value;
+    localStorage.setItem('noon', noon);
+    var onePm = document.getElementById('onePm').value;
+    localStorage.setItem('onePm', onePm);
+    var twoPm = document.getElementById('twoPm').value;
+    localStorage.setItem('twoPm', twoPm);
+    var threePm = document.getElementById('threePm').value;
+    localStorage.setItem('threePm', threePm);
+    var fourPm = document.getElementById('fourPm').value;
+    localStorage.setItem('fourPm', fourPm);
+    var fivePm = document.getElementById('fivePm').value;
+    localStorage.setItem('fivePm', fivePm);
+
 });
