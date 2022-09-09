ab246e491f8b138fdd2c670db51f31e79f5c9ac9
Roy <loganroyjr4@gmail.com>
2022-09-08 21:27:01 -0500

    learned how to use placholder haha

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// function to display time
+// THEN the current day is displayed at the top of the calendar
 
 function currentDay() {
     var day = moment().format('dddd');
@@ -9,14 +9,23 @@ function currentDay() {
 currentDay();
 
 
-
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
 // adding click event to save button ...function to save to local storage
 // also displays user test after save button clicked"
+
 $('.saveBtn').click(function(){
     var nineAm = document.getElementById('nineAm').value;
     localStorage.setItem('nineAm', nineAm);
     var displayNine = localStorage.getItem('nineAm');
     document.getElementById('nineAm').value = displayNine;
+
     var tenAm = document.getElementById('tenAm').value;
     localStorage.setItem('tenAm', tenAm);
     var displayTen = localStorage.getItem('tenAm');
@@ -52,18 +61,9 @@ $('.saveBtn').click(function(){
 
 });
 
-// ```md
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
+
+
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
+//  var hour = getHours();
+//  console.log(hour);
